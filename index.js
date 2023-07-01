58206eb45feb0317cb8bafcb6bd5365231ac8f10
Woody Lin <wancheng.lin001@umb.edu>
2023-06-30 23:23:19 -0400

    Update index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+const { token } = require('./config.json')
+const { Client, Events, GatewayIntentBits, SlashCommandBuilder } = require('discord.js')
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] })
+
+client.once(Events.ClientReady, c => {
+    console.log(`Logged in as ${c.user.tag}`);
+
+    const ping = new SlashCommandBuilder()
+        .setName('who')
+        .setDescription('汪汪汪');
+
+    const hello = new SlashCommandBuilder()
+        .setName('hello')
+        .setDescription('打个招呼吧');
+
+    client.application.commands.create(ping, "980887444431978517")
+    client.application.commands.create(hello, "980887444431978517")
+})
+
+client.on(Events.InteractionCreate, interaction => {
+    if(!interaction.isChatInputCommand()) return;
+    if(interaction.commandName === "who") {
+        interaction.reply("我是山里灵活的狗");
+    }
+    if(interaction.commandName === "hello") {
+        interaction.reply(`Hello ${interaction.user.username}!`);
+    }
+    console.log(interaction);
+});
+
+client.login(token);
