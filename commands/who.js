490e279c6218e2683e5577b8d923794f7d468c5b
Woody Lin <wancheng.lin001@umb.edu>
2023-07-12 16:57:53 -0400

    ...

--- commands/who.js
+++ commands/who.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder } = require("discord.js");
 module.exports = {
     data: new SlashCommandBuilder()
     .setName('who')
-    .setDescription('汪汪汪'),
+    .setDescription('猜猜我是谁'),
 
     async execute(interaction){
         interaction.reply("我是山里灵活的狗");
